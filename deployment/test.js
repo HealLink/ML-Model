1ff1378b62a4b6db792cb9161af6e291d96b7e51
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2024-12-13 13:15:41 +0700

    update

--- deployment/test.js
+++ deployment/test.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const TF_SERVING_URL = "http://localhost:8501/v1/models/model_bert_en_uncased_L-12_H-768_A-12:predict";
+const TF_SERVING_URL = "https://ml-model-serving-26979933732.asia-southeast2.run.app/v1/models/model_bert-en-uncased-l-12-h-768-a-12:predict";
 
 const inputData = {
   instances: [
